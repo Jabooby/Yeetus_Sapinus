847de77d5b3d3284e5f00f7ce32f3da4a29af224
Charles-cad <charlesarieldion@gmaIl.com>
2024-07-11 10:30:23 -0400

    template prof

--- Arduino/Arduino/src/main.cpp
+++ Arduino/Arduino/src/main.cpp
@@ -1,64 +1,217 @@
-#include <Arduino.h>
-#include <librobus.h>
+/* 
+ * GRO 302 - Conception d'un robot mobile
+ * Code de démarrage
+ * Auteurs: Jean-Samuel Lauzon     
+ * date: 1 mai 2019
+*/
 
-// Constants and variables
-//
-const uint8_t Electromagnet_pin[2] = {SCL, SDA}; //J10
-int pinJ19 = A5;
-int pot;
-float angle;
+/*------------------------------ Librairies ---------------------------------*/
+#include <LibS3GRO.h>
+#include <ArduinoJson.h>
+#include <libExample.h> // Vos propres librairies
+/*------------------------------ Constantes ---------------------------------*/
 
-// put function declarations here:
-float readpot( int pin );
-void electromagnet_control( uint8_t state,  unsigned int duration );
+#define BAUD            115200      // Frequence de transmission serielle
+#define UPDATE_PERIODE  100         // Periode (ms) d'envoie d'etat general
+
+#define MAGPIN          32          // Port numerique pour electroaimant
+#define POTPIN          A5          // Port analogique pour le potentiometre
+
+#define PASPARTOUR      64          // Nombre de pas par tour du moteur
+#define RAPPORTVITESSE  50          // Rapport de vitesse du moteur
+
+/*---------------------------- variables globales ---------------------------*/
+
+ArduinoX AX_;                       // objet arduinoX
+MegaServo servo_;                   // objet servomoteur
+VexQuadEncoder vexEncoder_;         // objet encodeur vex
+IMU9DOF imu_;                       // objet imu
+PID pid_;                           // objet PID
+
+volatile bool shouldSend_ = false;  // drapeau prêt à envoyer un message
+volatile bool shouldRead_ = false;  // drapeau prêt à lire un message
+volatile bool shouldPulse_ = false; // drapeau pour effectuer un pulse
+volatile bool isInPulse_ = false;   // drapeau pour effectuer un pulse
+
+SoftTimer timerSendMsg_;            // chronometre d'envoie de messages
+SoftTimer timerPulse_;              // chronometre pour la duree d'un pulse
+
+uint16_t pulseTime_ = 0;            // temps dun pulse en ms
+float pulsePWM_ = 0;                // Amplitude de la tension au moteur [-1,1]
+
+
+float Axyz[3];                      // tableau pour accelerometre
+float Gxyz[3];                      // tableau pour giroscope
+float Mxyz[3];                      // tableau pour magnetometre
+
+/*------------------------- Prototypes de fonctions -------------------------*/
+
+void timerCallback();
+void startPulse();
+void endPulse();
+void sendMsg(); 
+void readMsg();
+void serialEvent();
+
+// Fonctions pour le PID
+double PIDmeasurement();
+void PIDcommand(double cmd);
+void PIDgoalReached();
+
+/*---------------------------- fonctions "Main" -----------------------------*/
 
 void setup() {
-  pinMode(Electromagnet_pin, OUTPUT);
-  //BoardInit(); //faut trouver façon de just init le arduino X mais jsp programmer objet faque j'ai pas réussi
-  Serial.begin(9600);
+  Serial.begin(BAUD);               // initialisation de la communication serielle
+  AX_.init();                       // initialisation de la carte ArduinoX 
+  imu_.init();                      // initialisation de la centrale inertielle
+  vexEncoder_.init(2,3);            // initialisation de l'encodeur VEX
+  // attache de l'interruption pour encodeur vex
+  attachInterrupt(vexEncoder_.getPinInt(), []{vexEncoder_.isr();}, FALLING);
+  
+  // Chronometre envoie message
+  timerSendMsg_.setDelay(UPDATE_PERIODE);
+  timerSendMsg_.setCallback(timerCallback);
+  timerSendMsg_.enable();
+
+  // Chronometre duration pulse
+  timerPulse_.setCallback(endPulse);
+  
+  // Initialisation du PID
+  pid_.setGains(0.25,0.1 ,0);
+  // Attache des fonctions de retour
+  pid_.setMeasurementFunc(PIDmeasurement);
+  pid_.setCommandFunc(PIDcommand);
+  pid_.setAtGoalFunc(PIDgoalReached);
+  pid_.setEpsilon(0.001);
+  pid_.setPeriod(200);
 }
 
+/* Boucle principale (infinie)*/
 void loop() {
-  
-  //Lecture du potentiometre
-  pot = analogRead(pinJ19);
-  
-  //Utilisation de la fonction de calcul de l'angle
-  angle = readpot( pinJ19 );
 
-  //Print les résultats
-  Serial.print(pot);
-  Serial.print("---");
-  Serial.print(angle);
-  Serial.print("\n");
-  delay(1000);
-
-}
-
-
-//SECTIONS POUR LES FONCTIONS 
-float readpot( int pin ) {
-
-  // la pin de donnée pour J19 est : A5
-
-  float offset_0 = 132;
-  float angle = ((float)analogRead(pin) / 1023.0) * 265.0 - offset_0;
-  return angle;
-
-}
-
-void electromagnet_control( uint8_t state,  unsigned long duration ) {
-  //Active ou non l'électroaimant
-  digitalWrite(Electromagnet_pin, state);
-
-  //Ecrit le state de l'électroaimant
-  if (state == HIGH) {
-      Serial.println("Electromagnet is ON");
-  } else {
-      Serial.println("Electromagnet is OFF");
+  if(shouldRead_){
+    readMsg();
+  }
+  if(shouldSend_){
+    sendMsg();
+  }
+  if(shouldPulse_){
+    startPulse();
   }
 
-  //Durée de l'activation
-  delay(duration);
+  // mise a jour des chronometres
+  timerSendMsg_.update();
+  timerPulse_.update();
+  
+  // mise à jour du PID
+  pid_.run();
+}
 
+/*---------------------------Definition de fonctions ------------------------*/
+
+void serialEvent(){shouldRead_ = true;}
+
+void timerCallback(){shouldSend_ = true;}
+
+void startPulse(){
+  /* Demarrage d'un pulse */
+  timerPulse_.setDelay(pulseTime_);
+  timerPulse_.enable();
+  timerPulse_.setRepetition(1);
+  AX_.setMotorPWM(0, pulsePWM_);
+  AX_.setMotorPWM(1, pulsePWM_);
+  shouldPulse_ = false;
+  isInPulse_ = true;
+}
+
+void endPulse(){
+  /* Rappel du chronometre */
+  AX_.setMotorPWM(0,0);
+  AX_.setMotorPWM(1,0);
+  timerPulse_.disable();
+  isInPulse_ = false;
+}
+
+void sendMsg(){
+  /* Envoit du message Json sur le port seriel */
+  StaticJsonDocument<500> doc;
+  // Elements du message
+
+  doc["time"] = millis();
+  doc["potVex"] = analogRead(POTPIN);
+  doc["encVex"] = vexEncoder_.getCount();
+  doc["goal"] = pid_.getGoal();
+  doc["measurements"] = PIDmeasurement();
+  doc["voltage"] = AX_.getVoltage();
+  doc["current"] = AX_.getCurrent(); 
+  doc["pulsePWM"] = pulsePWM_;
+  doc["pulseTime"] = pulseTime_;
+  doc["inPulse"] = isInPulse_;
+  doc["accelX"] = imu_.getAccelX();
+  doc["accelY"] = imu_.getAccelY();
+  doc["accelZ"] = imu_.getAccelZ();
+  doc["gyroX"] = imu_.getGyroX();
+  doc["gyroY"] = imu_.getGyroY();
+  doc["gyroZ"] = imu_.getGyroZ();
+  doc["isGoal"] = pid_.isAtGoal();
+  doc["actualTime"] = pid_.getActualDt();
+
+  // Serialisation
+  serializeJson(doc, Serial);
+  // Envoit
+  Serial.println();
+  shouldSend_ = false;
+}
+
+void readMsg(){
+  // Lecture du message Json
+  StaticJsonDocument<500> doc;
+  JsonVariant parse_msg;
+
+  // Lecture sur le port Seriel
+  DeserializationError error = deserializeJson(doc, Serial);
+  shouldRead_ = false;
+
+  // Si erreur dans le message
+  if (error) {
+    Serial.print("deserialize() failed: ");
+    Serial.println(error.c_str());
+    return;
+  }
+  
+  // Analyse des éléments du message message
+  parse_msg = doc["pulsePWM"];
+  if(!parse_msg.isNull()){
+     pulsePWM_ = doc["pulsePWM"].as<float>();
+  }
+
+  parse_msg = doc["pulseTime"];
+  if(!parse_msg.isNull()){
+     pulseTime_ = doc["pulseTime"].as<float>();
+  }
+
+  parse_msg = doc["pulse"];
+  if(!parse_msg.isNull()){
+     shouldPulse_ = doc["pulse"];
+  }
+  parse_msg = doc["setGoal"];
+  if(!parse_msg.isNull()){
+    pid_.disable();
+    pid_.setGains(doc["setGoal"][0], doc["setGoal"][1], doc["setGoal"][2]);
+    pid_.setEpsilon(doc["setGoal"][3]);
+    pid_.setGoal(doc["setGoal"][4]);
+    pid_.enable();
+  }
+}
+
+
+// Fonctions pour le PID
+double PIDmeasurement(){
+  // To do
+}
+void PIDcommand(double cmd){
+  // To do
+}
+void PIDgoalReached(){
+  // To do
 }
